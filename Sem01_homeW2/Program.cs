617e192c31b6a6060329aa6bfba54c7fa8991dab
Nikita_Kh <t51-2@ya.ru>
2023-02-18 23:47:35 +0700

    Домашняя задача 2

---
+++ Sem01_homeW2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход
+// три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int Max = numberA;
+if (numberB > Max)
+{
+    Max = numberB;
+}
+if (numberC > Max)
+{
+    Max = numberC;
+}
+System.Console.WriteLine(Max + " - Максимальное число");
