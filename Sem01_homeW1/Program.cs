e72d63e254b301626acb4657d13ec61173b51624
Nikita_Kh <t51-2@ya.ru>
2023-02-18 21:48:17 +0700

    Домашняя задача 1

---
+++ Sem01_homeW1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+System.Console.WriteLine(numberA + " - Большее число");
+System.Console.WriteLine(numberB + " - Меньшее число");
+}
+else
+{
+    System.Console.WriteLine(numberB + " - Большее число");
+    System.Console.WriteLine(numberA + " - Меньшее число");
+}
