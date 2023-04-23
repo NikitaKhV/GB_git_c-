e2dae92ce73e2ebea58462f16f53b1be79e2af60
Nikita_Kh <t51-2@ya.ru>
2023-04-23 15:38:00 +0700

    Sem04_homeW3 - Задача №29

---
+++ Sem04_homeW3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] FillArray(int arrayLength, int leftSide, int rightSide)
+{
+    Random rand = new Random();
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftSide, rightSide);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+int[] array = FillArray(8, -3, 20);
+PrintArray(array);
