b5acc84d15aa94bab7caaf015c519c6206e558f3
Nikita_Kh <t51-2@ya.ru>
2023-05-20 17:45:20 +0700

    Sem06_homeW2 - Задача №43

---
+++ Sem06_homeW2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5,
+// b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double Prompt(string message)
+{
+    Console.Write(message);
+    double num = Convert.ToDouble(Console.ReadLine()!);
+    return num;
+}
+void GetPoint(double k1, double b1, double k2, double b2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+
+    double y1 = k1 * x + b1;
+    double y2 = k2 * x + b2;
+
+    Console.Write($" -> ({y1}; {y2})");
+}
+double b1 = Prompt($"Введите значение b1: ");
+double k1 = Prompt($"Введите значение k1: ");
+double b2 = Prompt($"Введите значение b2: ");
+double k2 = Prompt($"Введите значение k2: ");
+Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} ");
+GetPoint(k1, b1, k2, b2);
