ed218864390d0774a75422c6c7b182db9786a1f9
Nikita_Kh <t51-2@ya.ru>
2023-05-20 17:04:28 +0700

    Sem06_homeW1 - Задача №41

---
+++ Sem06_homeW1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите числа через пробел: ");
+int[] myArray = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+int count = 0;
+int GenerateArray(int[] myArray)
+{
+    for (int i = 0; i < myArray.Length; i++)
+        if (myArray[i] > 0)
+        {
+            count++;
+        }
+    return count;
+}
+GenerateArray(myArray);
+PrintArray(myArray);
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"Кол-во элементов > 0: - {count}");
+}
