4013810f4453c0502caf0b001cae49d9cd4f185e
Nikita_Kh <t51-2@ya.ru>
2023-05-27 17:26:28 +0700

    Sem08_homeW4 - Задача №61

---
+++ Sem08_homeW4/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задача 61: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int[,] CreateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 5);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] MultiplyMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int[,] matrix3 = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix2.GetLength(0); k++)
+            {
+                matrix3[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+        }
+    }
+    return matrix3;
+}
+
+int[,] matrix1 = CreateMatrix(2, 2);
+PrintMatrix(matrix1);
+System.Console.WriteLine();
+int[,] matrix2 = CreateMatrix(2, 2);
+PrintMatrix(matrix2);
+System.Console.WriteLine();
+int[,] matrix3 = MultiplyMatrix(matrix1, matrix2);
+PrintMatrix(matrix3);
