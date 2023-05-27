584f4f5314cbffd5a215c96b036735f69341b18c
Nikita_Kh <t51-2@ya.ru>
2023-05-27 17:43:02 +0700

    Sem09_homeW1 - Задача №64

---
+++ Sem09_homeW1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void WriteNumbers(int num)
+{
+    if (num == 0) return;
+    System.Console.Write(num + " ");
+    WriteNumbers(num - 1);
+}
+
+System.Console.WriteLine();
+int n = ReadInt("Введите значение - N: ");
+System.Console.Write($"Натуральные числа по убыванию {n} to 1: ");
+WriteNumbers(n);
