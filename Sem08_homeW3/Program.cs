657359fa2e6f57c6c2131e06d8782e0f99539ac9
Nikita_Kh <t51-2@ya.ru>
2023-05-27 17:00:39 +0700

    Sem08_homeW3 - Задача №58

---
+++ Sem08_homeW3/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 58: Заполните спирально массив 4 на 4 числами от 1 до 16.
+
+void FillArray(int[,] array, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int l = 0; l < n * n; l++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; }
+        while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 10)
+            {
+                Console.Write("0" + array[i, j]);
+                Console.Write(" ");
+            }
+            else Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int dimen = 4;
+int[,] matrix = new int[dimen, dimen];
+FillArray(matrix, dimen);
+PrintArray(matrix);
